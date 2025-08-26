fef77d26838bd827d560226d0536e62f1397c8c5
Grace Jiang <grji@odoo.com>
2025-08-26 16:30:08 -0700

    working on dashboard

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,12 +1,24 @@
 /** @odoo-module **/
 
-/**test  */
-
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = {Layout };
+    static props = {};
+
+
+    setup() {
+        this.action = useService("action");
+    }
+    openCustomers() {
+        this.action.doAction("base.action_partner_form");
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+
+
