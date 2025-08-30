d5175c7588bfd6442765356becfdb75583ddf8b4
Grace Jiang <grji@odoo.com>
2025-08-29 17:21:34 -0700

    create a client action

--- awesome_clicker/static/src/ clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/ clicker_systray_item/clicker_systray_item.js
@@ -1,5 +1,6 @@
 import { registry } from "@web/core/registry";
 import { Component, useState, useExternalListener } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
 
 export class ClickerSystray extends Component {
     static template = "awesome_clicker.ClickerSystray";
@@ -8,10 +9,20 @@ export class ClickerSystray extends Component {
     setup() {
         this.state = useState({ counter: 0 });
         useExternalListener(document.body, "click", ()=>this.state.counter++, true);
+        this.action = useService("action")
     }
 
     increment() {
         this.state.counter+= 9;
+    }
+
+    openClientAction() {
+        this.action.doAction({
+            type: "ir.actions.client",
+            tag: "awesome_clicker.client_action",
+            target: "new",
+            name: "Clicker Game"
+        });
     }
 
 }
