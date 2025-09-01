1ab787eb5c8b2f97b95f6235b016c2a945673a6d
Grace Jiang <grji@odoo.com>
2025-08-31 22:17:41 -0700

    clicker 3.6

--- awesome_clicker/static/src/client_action/client_action.js
+++ awesome_clicker/static/src/client_action/client_action.js
@@ -2,10 +2,12 @@ import { registry } from "@web/core/registry";
 import { Component, useState } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { useClicker } from "../clicker_hook";
+import { ClickerValue } from "../clicker_value/clicker_value";
 
 export class ClickerClientAction extends Component {
     static template = "awesome_clicker.ClickerClientAction"; 
     static props = ["*"];
+    static components = { ClickerValue}
     setup() {
         //this.clickService = useState(useService("awesome_clicker.clicker"));
         this.clicker = useClicker();
